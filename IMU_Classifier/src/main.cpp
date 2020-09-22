39b0377f138ae1eb30309f11720dc82e444f8eee
Christopher Ta <39348633+Digital1O1@users.noreply.github.com>
2020-09-21 19:08:33 -0500

    Add files via upload

---
+++ IMU_Classifier/src/main.cpp
@@ -0,0 +1,16 @@
+#include <Arduino.h>
+#include <TensorFlowLite.h>
+#include <tensorflow/lite/experimental/micro/kernels/all_ops_resolver.h>
+#include <tensorflow/lite/experimental/micro/micro_error_reporter.h>
+#include <tensorflow/lite/experimental/micro/micro_interpreter.h>
+#include <tensorflow/lite/schema/schema_generated.h>
+#include <tensorflow/lite/version.h>
+void setup() 
+{
+  // put your setup code here, to run once:
+}
+
+void loop()
+ {
+  // put your main code here, to run repeatedly:
+}
